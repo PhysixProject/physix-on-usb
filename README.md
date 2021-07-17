a4e38a594096bb64b43aeaa206799453e459d7f7
Tree Davies <tdavies@darkphysics.net>
2021-07-17 12:20:28 -0700

    README and project setup.
    Signed-off-by: Tree Davies <tdavies@darkphysics.net>

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # physix-on-usb
+
+The purpose of this repo is to maintain and document the process
+of building a bootable Physix Project USB
+
+
+## Config ##
+Add paths to the kernel, initrd, and squash filesstem to config file
+located at root of this repo.
+* If you have not built your kernel, see instructions below.
+* If you have not created your SquashFS image, see section below.
+
+
+## Build ISO image ##
+Run: ./build-iso.sh
+
+
+##  Comfig/compile kernel ##
+```
+git clone git://github.com/sfjro/aufs5-standalone.git
+cd aufs5-standalone
+git checkout aufs5.4.3
+cp aufs5-*.patch ..
+rm -f include/uapi/linux/Kbuild
+tar cvfz aufs5.tar.gz Documentation fs include
+cp aufs5.tar.gz ..
+cd ..
+
+wget https://mirrors.edge.kernel.org/pub/linux/kernel/v5.x/linux-5.4.41.tar.gz
+tar xf linux-5.4.41.tar.gz
+cd linux-5.4.41
+cp /opt/admin/physix/build-scripts/03-base-config/configs/linux_config-5.4.41 .
+cat ../aufs5-base.patch | patch -Np1
+cat ../aufs5-kbuild.patch | patch -Np1
+cat ../aufs5-mmap.patch | patch -Np1
+cat ../aufs5-standalone.patch | patch -Np1
+tar xvf ../aufs5.tar.gz
+
+make menuconfig
+```
+When you configure the kernel, make sure the following are enabled as builtins (not as modules):
+* SQUASHFS support (and support for SQUASHFS XZ compressed file systems).
+* UNIONFS or AUFS support (Teo En Ming *did not* patch Linux Kernel
+* AUFS or UNIONFS support
+* CDROM support (ISO9660).
+* DEVTMPFS support.
+* cgroups
+
+```
+make -j8
+make modules
+make modules_install
+make headers_install
+```
+
+## Create SquashFS Image ##
+TODO
+
+
+
+
+
+## Troubleshooting ##
+* Issue: Kernel boots, bu switchroot fails.
+* Solution: Mount it manually.
+```
+mkdir -p /tmp/SR0
+mount /dev/sr0 /tmp/SR0
+if [ -e /tmp/SR0/live/filesystem.squashfs ] ; then
+	mount /tmp/SR0/live/filesystem.squashfs /sysroot
+fi
+```
+
---
+++ build-iso.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+source ./config
+
+if [ ! -r ./syslinux-6.04-pre1.tar.gz ] ; then
+	wget https://mirrors.edge.kernel.org/pub/linux/utils/boot/syslinux/Testing/6.04/syslinux-6.04-pre1.tar.gz
+	tar xf syslinux-6.04-pre1.tar.gz
+fi
+
+cp -v ./syslinux-6.04-pre1/bios/core/isolinux.bin                  ./physix-project-usb/isolinux/ &&
+cp -v ./syslinux-6.04-pre1/bios/com32/hdt/hdt.c32                  ./physix-project-usb/isolinux/ &&
+cp -v ./syslinux-6.04-pre1/bios/com32/elflink/ldlinux/ldlinux.c32  ./physix-project-usb/isolinux/ &&
+cp -v ./syslinux-6.04-pre1/bios/com32/lib/libcom32.c32             ./physix-project-usb/isolinux/ &&
+cp -v ./syslinux-6.04-pre1/bios/com32/gpllib/libgpl.c32            ./physix-project-usb/isolinux/ &&
+cp -v ./syslinux-6.04-pre1/bios/com32/cmenu/libmenu/libmenu.c32    ./physix-project-usb/isolinux/ &&
+cp -v ./syslinux-6.04-pre1/bios/com32/cmenu/libmenu/libmenu.c32    ./physix-project-usb/isolinux/ &&
+cp -v ./syslinux-6.04-pre1/bios/com32/libutil/libutil.c32          ./physix-project-usb/isolinux/ &&
+cp -v ./syslinux-6.04-pre1/bios/com32/menu/vesamenu.c32            ./physix-project-usb/isolinux/ &&
+cp -v ./syslinux-6.04-pre1/bios/memdisk/memdisk                    ./physix-project-usb/isolinux/ 
+if [ $? -ne 0 ] ; then
+	echo "Error: cp syslinux files"
+	exit 1
+fi
+
+if [ $KERNEL ] ; then
+	cp -v $KERNEL ./physix-project-usb/live/vmlinuz.x86_64
+else
+	echo "No exported KERNEL"
+	exit 1
+fi
+
+if [ $INITRD ] ; then
+    cp -v $INITRD ./physix-project-usb/live/initrd-img.x86_64
+else
+	echo "No exported INITRD"
+	exit 1
+fi
+
+if [ $SQUASH_FS ] ; then
+    cp -v $SQUASH_FS ./physix-project-usb/live
+else
+	echo "No exported SQUASH_FS"
+	exit 1
+fi
+
+genisoimage -o "Physix-Project-Beta.iso" \
+            -v -J -R -D   \
+            -A "Physix-Project"   \
+            -V Physix-Project   \
+            -no-emul-boot \
+            -boot-info-table \
+            -boot-load-size 4   \
+            -b isolinux/isolinux.bin  \
+            -c isolinux/isolinux.boot physix-project-usb
+
+./syslinux-6.04-pre1/bios/utils/isohybrid.pl  Physix-Project-Beta.iso
+
+
+
